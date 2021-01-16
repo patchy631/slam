64868ce9c04a47d1930ef7e1cd13a3b9ed861b31
Akshay Pachaar <akshay.pachaar@tomtom.com>
2021-01-16 22:57:16 +0530

    adding extractor.py which extracts and refines the tracking

--- slam.py
+++ slam.py
@@ -1,14 +1,27 @@
 import cv2
 import time
+import numpy as np
 from display import Display
+from extrcator import Extractor
 
 W = 1920//2
 H = 1080//2
 
 disp = Display(W, H)
 
+fe = Extractor()
+
 def process_frame(img):
     img = cv2.resize(img, (W, H))
+    matches = fe.extract(img)
+    if matches is None:
+        return
+    for pt1, pt2 in matches:
+        u1, v1 = map(lambda x: int(round(x)), pt1)
+        u2, v2 = map(lambda x: int(round(x)), pt2)
+        cv2.circle(img, center=(u1, v1), radius=3, color=(0, 255, 0))
+        cv2.line(img, (u1, v1), (u2, v2), color=(0, 0, 255))
+
     disp.paint(img)
 
 
